cc63584e9f82db08fd0aa7523ee49b834c24b4cf
Martin Roman <martin@localhost.localdomain>
2018-11-07 15:30:31 -0600

    Sistema completo V2

--- server.js
+++ server.js
@@ -44,22 +44,33 @@ app.use(express.static( __dirname + '/public' ));
 app.get('/', (req, res) => {
   console.log('Entro en GET /');
   res.sendFile( __dirname + '/index.html' );
-  
-  /*MongoClient.connect(url, function(err, client) {
-  if (err) throw err;
-  var dbo = client.db("QR"); 
-  dbo.collection('ID_Raspberry').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    // renders index.ejs
-    res.render('index.ejs', {codigos: result});
-    });
-  });*/
+});
+
+// Sesion (sesion.html)
+app.get('/sesion', (req, res) => {
+  console.log('Entro en GET /sesion');
+  //res.sendFile funciona para descargar archivos
+  //res.sendFile( __dirname + '/views/sesion.ejs' );
+  res.render('sesion.ejs', {mensaje: ""});
+});
+
+// QR Lector (video.html)
+app.get('/qr-code', (req, res) => {
+  console.log('Entro en GET /qr-code');
+  res.sendFile( __dirname + '/pages/video.html' );
+});
+
+// Registro (registro.html)
+app.get('/registro', (req, res) => {
+  console.log('Entro en GET /registro');
+  //res.sendFile( __dirname + '/pages/registro.html' );
+  res.render('registro.ejs', {mensaje: ""});
 });
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 // Validar datos en DB y dirigir a admin.html
-app.post('/inicio_sesion', (req, res) => {
+app.post('/sesion', (req, res) => {
   let body = req.body;
   let user = body.name;
   let pass = body.password;
@@ -79,12 +90,24 @@ app.post('/inicio_sesion', (req, res) => {
           //console.log(result);
           var pass_hash = result.password; 
           console.log(pass_hash);
-
+          client.close(); //Closing first query
           bcrypt.compare(pass, pass_hash, function(err, out) {
             if( out === true){
               console.log('Password coinciden');
-              //res.redirect('/admin.html');
-              res.sendFile( __dirname + '/pages/admin.html' );
+
+              MongoClient.connect(url, function(err, client) {
+                if (err) throw err;
+                var dbo = client.db("id_qr"); 
+              dbo.collection("ID_Raspberry").find({}).toArray(function(err, result) {
+                if (err) throw err;
+                else{
+                    res.render('admin.ejs', {mensaje: "", codigos: result});
+                  }
+                });
+                client.close();
+                });
+
+              //res.sendFile( __dirname + '/pages/admin.html' );
             }else{
               console.log('Password no coinciden');
               res.render('sesion.ejs', {mensaje: "Usuario y/o contraseña no validos"});
@@ -99,7 +122,7 @@ app.post('/inicio_sesion', (req, res) => {
         }
       }
     });
-    client.close();
+    //client.close();
   });
 
   //res.redirect('/');
@@ -108,54 +131,57 @@ app.post('/inicio_sesion', (req, res) => {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-// Dirigir a registro.html
-app.post('/registro_page', (req, res) => {
-  //let codigo = req.body.codigo;
-  //res.redirect('/registro.html')
-  res.sendFile( __dirname + '/pages/registro.html' );
-});
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 // Registro 
 app.post('/registro', (req, res) => {
+  
   let body = req.body;
+  console.log(body);
   let user = body.name;
   let pass = body.password;
+  let pass_again = body.passwordagain;
 
-  console.log(body);
+  var n = pass.localeCompare(pass_again);
 
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(pass, salt, function(err, hash) {
-      var userobj = { usuario: user }
-      var myobj = { usuario: user, password: hash };
-      console.log(hash);
-      
-      
-      MongoClient.connect(url, function(err, client) {
-        if (err) throw err;
-        var dbo = client.db("id_qr"); 
-    
-        dbo.collection("Users").find(userobj).toArray(function(err, result) {
+  if(n == 0){
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(pass, salt, function(err, hash) {
+        var userobj = { usuario: user }
+        var myobj = { usuario: user, password: hash };
+        console.log(hash);
+        
+        
+        MongoClient.connect(url, function(err, client) {
           if (err) throw err;
-          else{
-            if (result.length){ 
-              console.log('Usuario existente');
-              res.render('registro.ejs', {mensaje: "Usuario ya existente, ingrese uno nuevo."});
-            } else{
-              console.log('Usuario nuevo');
-              dbo.collection("Users").insertOne(myobj, function(err, result) {
-                if (err) throw err;
-                console.log('saved to database');
-              });
-              client.close();
-              res.redirect('/sesion.html');
+          var dbo = client.db("id_qr"); 
+      
+          dbo.collection("Users").find(userobj).toArray(function(err, result) {
+            if (err) throw err;
+            else{
+              if (result.length){ 
+                console.log('Usuario existente');
+                res.render('registro.ejs', {mensaje: "Usuario ya existente, ingrese uno nuevo."});
+              } else{
+                console.log('Usuario nuevo');
+                dbo.collection("Users").insertOne(myobj, function(err, result) {
+                  if (err) throw err;
+                  console.log('saved to database');
+                });
+                client.close();
+                res.redirect('/sesion');
+              }
             }
-          }
+          });
         });
       });
     });
-  });
+  } else{
+    res.render('registro.ejs', {mensaje: "Las contraseñas no coinciden."});
+  }
+
 
 });
 
@@ -245,7 +271,7 @@ app.post('/quotes', (req, res) => {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-// Obtener valores con método GET
+// Búsqueda de códigos
 app.post('/buscar', (req, res) => {
   //let codigo = req.body.codigo;
   let objcodigo = req.body;
@@ -262,9 +288,23 @@ app.post('/buscar', (req, res) => {
           console.log('Código encontrado');
           res.render('admin.ejs', {mensaje: "", codigos: result});
         } else{
+          client.close(); //Closing last connection
           console.log('Código no encontrado');
+
+          MongoClient.connect(url, function(err, client) {
+            if (err) throw err;
+            var dbo = client.db("id_qr"); 
+          dbo.collection("ID_Raspberry").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            else{
+                res.render('admin.ejs', {mensaje: "No existe código en la DB", codigos: result});
+              }
+            });
+            client.close();
+            });
+
           //res.redirect('/admin.html');
-          res.render('admin.ejs', {mensaje: "No existe código en la DB", codigos: ""});
+          //res.render('admin.ejs', {mensaje: "No existe código en la DB", codigos: ""});
           //res.sendFile( __dirname + '/admin.html' );
         }
       }
@@ -276,6 +316,24 @@ app.post('/buscar', (req, res) => {
 
 });
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// Mostrar todos los códigos
+/*app.post('/buscar-todos', (req, res) => {
+
+  MongoClient.connect(url, function(err, client) {
+    if (err) throw err;
+    var dbo = client.db("id_qr"); 
+  dbo.collection("ID_Raspberry").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    else{
+        res.render('admin.ejs', {mensaje: "", codigos: result});
+      }
+    });
+    client.close();
+    });
+
+});*/
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
